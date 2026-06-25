1f4afddc0100ab1acabc3f52d8b2003a4df3eab1
Rochak Jain <rochakjain71@gmail.com>
2026-06-25 10:17:43 +0530

    frontend deploy

--- client/vite-project/src/utils/axios.js
+++ client/vite-project/src/utils/axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
-})
+});
+
+export default api;
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
