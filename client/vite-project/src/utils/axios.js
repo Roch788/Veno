a75b47cd72dd650a594414eb38ca485c4c510c6f
Rochak Jain <rochakjain71@gmail.com>
2026-06-25 10:08:47 +0530

    fixed backend

--- client/vite-project/src/utils/axios.js
+++ client/vite-project/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: "https://veno.onrender.com/api",
+    baseURL: 'http://localhost:8000/api',
     headers: {
         "Content-Type": "application/json",
     },
